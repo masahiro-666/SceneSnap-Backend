f8324ce7e69d342cbedbc1b2ab35c4602134761c
masahiro-666 <masahirokenta666@gmail.com>
2024-12-05 01:54:18 +0700

    feat: booking get each user

--- route/Booking.js
+++ route/Booking.js
@@ -10,13 +10,26 @@ router.get('/get', (req, res) => {
   })
 })
 
+router.get('/getEachUser/:customer_id', (req, res) => {
+  const customerId = req.params.customer_id;
+  const sql = "SELECT * FROM booking WHERE customer_id = ?";
+  
+  db.getConnection().query(sql, [customerId], (err, result) => {
+    if (err) return res.json({ Message: "Error fetching user data" });
+    if (result.length === 0) return res.json({ Message: "User not found" });
+    return res.json(result); // Return all bookings
+  });
+});
+
+
 router.post('/add', (req, res) => {
-  const sql = "INSERT INTO booking (`customer_id`, `movie_id`, `booking_seat`) VALUES (?)";
-  const { customer_id, movie_id, booking_seat } = req.body;
+  const sql = "INSERT INTO booking (`customer_id`, `movie_id`, `booking_seat`, `booking_price`) VALUES (?)";
+  const { customer_id, movie_id, booking_seat, booking_price} = req.body;
   const values = [
     customer_id,
     movie_id,
     booking_seat,
+    booking_price,
   ]
   db.getConnection().query(sql, [values], (err, result) => {
     if(err) return res.json(err);
